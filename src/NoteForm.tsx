9950391033e434fd60f43c82a755c99243dca85f
Promtada kuniart <6810110220@psu.ac.th>
2025-12-16 13:44:30 +0700

    BASE

---
+++ src/NoteForm.tsx
@@ -0,0 +1,10 @@
+const NoteForm = () => {
+  return (
+    <div style={{ marginBottom: '20px' }}>
+        <h2>Form Component</h2>
+        {/* เราจะมาเขียนฟอร์มกันใน Phase ต่อไป */}
+    </div>
+  )
+}
+
+export default NoteForm
---
+++ src/NoteList.tsx
@@ -0,0 +1,10 @@
+const NoteList = () => {
+  return (
+    <div>
+        <h2>List Component</h2>
+        {/* เราจะมาเขียนลิสต์กันใน Phase ต่อไป */}
+    </div>
+  )
+}
+
+export default NoteList
